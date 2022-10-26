6441576c77bb9923ffa68f2e64279fb8fd61f760
Robert Jordan <robert@twingate.com>
2022-10-26 16:00:18 -0700

    feat(direnv): Add auto_whitelist_workspaces option.

--- src/direnv/install.sh
+++ src/direnv/install.sh
@@ -92,8 +92,3 @@ if [ ${AUTO_ALLOW_WORKSPACES} == "true" ]; then
   echo "[whitelist]" > /etc/direnv.toml
   echo 'prefix = [ "/workspaces" ]' >> /etc/direnv.toml
 fi
-
-echo "***** BEGIN DEBUG OUTPUT *****"
-echo "AUTO_ALLOW_WORKSPACES=${AUTO_ALLOW_WORKSPACES}"
-cat /etc/direnv.toml
-echo "***** END DEBUG OUTPUT *****"
