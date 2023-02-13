b55ebfeaeacec4e3150f488f5314d05f5300b656
Robert Jordan <39421615+rojopolis@users.noreply.github.com>
2023-02-13 20:37:54 +0000

    Support architectures for awscli.

--- src/aws-cli/install.sh
+++ src/aws-cli/install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #
 _tmpdir=$(mktemp -d)
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "${_tmpdir}/awscliv2.zip"
+_arch=$(uname -m)
+curl "https://awscli.amazonaws.com/awscli-exe-linux-${_arch}.zip" -o "${_tmpdir}/awscliv2.zip"
 pushd ${_tmpdir}
 unzip awscliv2.zip
 ./aws/install
